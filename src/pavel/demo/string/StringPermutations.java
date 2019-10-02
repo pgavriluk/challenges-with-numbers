ff968c7cb822bc39c23b90eb00a1574cfd78ee75
Pavel <pavel.gavriluk@collibra.com>
2019-10-02 16:28:48 -0400

    Added string permutations example.

---
+++ src/pavel/demo/string/StringPermutations.java
@@ -0,0 +1,24 @@
+package pavel.demo.string;
+
+public class StringPermutations {
+
+	public static void main(String args[]) {
+		permutation("123");
+	}
+
+	private static void permutation(String input) {
+		permutation("", input);
+	}
+
+	private static void permutation(String perm, String str) {
+		if (str.isEmpty()) {
+			System.out.println(perm);
+		} else {
+			for (int i = 0; i < str.length(); i++){
+				permutation(perm + str.charAt(i), str.substring(0, i) + str.substring(i + 1));
+			}
+		}
+	}
+}
+
+
