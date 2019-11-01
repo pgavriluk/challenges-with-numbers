4882d1dc7bf7f07e5bab55a7783753b12f7c852d
Pavel <pgavriluk@gmail.com>
2019-11-01 12:51:53 -0400

    Added a full solution for Apple Stock challenge.

--- src/pavel/demo/challenges/AppleStockChallenge.java
+++ src/pavel/demo/challenges/AppleStockChallenge.java
@@ -5,23 +5,34 @@ public class AppleStockChallenge {
 	public static void main(String[] args) {
 
 
-		int[] stockPrices = new int[] {10, 7, 5, 8, 11, 9};
+		int[] stockPrices = new int[] {10, 7, 3, 8, 10, 9, 1, 3, 5, 9};
 
-		System.out.println("Max profit is:" + getMaxProfit(stockPrices));
+		System.out.println("Max profit is: " + getMaxProfit(stockPrices));
 	}
 
 	private static double getMaxProfit(int[] stockPrices){
 
 		int buyPrice = stockPrices[0];
-		int sellPrice = stockPrices[0];
+		int potentialBuyPrice = 0;
+		int sellPrice = stockPrices[1];
 
-		for (int timeSlot = 1; timeSlot < stockPrices.length; timeSlot++) {
+		for (int timeSlot = 1; timeSlot < stockPrices.length-1; timeSlot++) {
 			if(buyPrice > stockPrices[timeSlot]){
-				buyPrice = stockPrices[timeSlot];
+				potentialBuyPrice = stockPrices[timeSlot];
 			}
 
-			if(sellPrice < stockPrices[timeSlot]){
-				sellPrice = stockPrices[timeSlot];
+			int potentialSellPrice = stockPrices[timeSlot+1];
+
+			if(sellPrice < potentialSellPrice){
+				sellPrice = potentialSellPrice;
+				if(potentialBuyPrice < buyPrice){
+					buyPrice = potentialBuyPrice;
+				}
+			} else if(potentialBuyPrice < buyPrice){
+				if(potentialSellPrice - potentialBuyPrice > sellPrice - buyPrice){
+					buyPrice = potentialBuyPrice;
+					sellPrice = potentialSellPrice;
+				}
 			}
 
 		}
