f1057807662e73ed4f574bc68c19cf702babf897
Pavel <48726480+pgavriluk@users.noreply.github.com>
2020-08-27 09:41:15 -0400

    Create Palindrome.java

---
+++ src/pavel/demo/Palindrome.java
@@ -0,0 +1,32 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 1211121;
+
+        System.out.println(isPalindrome(x));
+
+    }
+
+    public static boolean isPalindrome(int x) {
+
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reverse = 0;
+        int original = x;
+
+
+        while (x != 0) {
+            int last = x % 10;
+            reverse = reverse * 10 + last;
+            x /= 10;
+        }
+
+
+        return original == reverse;
+    }
+
+}
